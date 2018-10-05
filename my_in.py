156f4d1bb385d6e13d8bf42e73f811d574e8c5ea
jonathantvrs <jhonathan200969@gmail.com>
2018-10-05 20:05:52 -0300

    my split and my in operator

---
+++ my_in.py
@@ -0,0 +1,8 @@
+# -*-  coding: utf-8  -*-
+# author: jonathantvrs
+
+def custom_in(list, parameter):
+    for element in list:
+        if element == parameter:
+            return True
+        return False
---
+++ my_split.py
@@ -0,0 +1,20 @@
+# -*-  coding: utf-8  -*-
+# author: jonathantvrs
+
+def custom_split(string, separator):
+    elements = []
+    string_aux = ""
+
+    for element in string:
+        if element != separator:
+            string_aux += element
+        else:
+            elements.append(string_aux)
+            string_aux = ""
+    if string_aux != "" or string_aux != separator:
+        elements.append(string_aux)
+    return elements
+
+#print custom_split("Jonathan Everton Wesley", " ")
+
+assert custom_split("Jonathan,Everton", ",") == ['Jonathan', 'Everton']
