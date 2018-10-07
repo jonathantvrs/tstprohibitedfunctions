f464b8aa92547adfc9520857f6ad76704e299b30
jonathantvrs <jhonathan200969@gmail.com>
2018-10-07 18:03:12 -0300

    add comments in function my replace for all cases. fix bug # string out of range

--- my_replace_for_all_cases.py
+++ my_replace_for_all_cases.py
@@ -2,17 +2,27 @@
 # author: jonathantvrs
 
 def custom_replace(string, parameter, new_parameter):
+    # variavel que recebe string com as trocas
     string_aux = ""
+    # variavel que recebe string procurada (parameter)
     searched_string = ""
+    # variavel de control do while
     index = 0
 
+    # laço que vai executar ate a variavel de controle atingir o tamanho da string
     while index < len(string):
+        # condição que testa se string na posicao index é igual a string procurada na posicao 0
         if string[index].lower() == parameter[0].lower():
+            # laço para iterar sobre a palavra procurada
             for sec_index in range(len(parameter)):
+                # condição que testa caracter a caracter da string com a procurada
                 if string[index + sec_index].lower() == parameter[sec_index].lower():
+                    # enquanto for igual, string procurada recebe string na posicao atual + a posicao do segundo for
                     searched_string += string[index + sec_index]
                 else:
+                    # se for diferente, a string é acumulada na nova string
                     string_aux += searched_string
+            # condicao que testa se a string procurada formada é igual a que é para ser substituida
             if searched_string.lower() == parameter.lower():
                 string_aux += new_parameter
                 searched_string = ""
