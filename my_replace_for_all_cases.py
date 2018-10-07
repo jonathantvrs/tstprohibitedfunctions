e0715102d5c3c00a86d5b52d6d42e63e860ebe74
jonathantvrs <jhonathan200969@gmail.com>
2018-10-07 03:20:42 -0300

    implementing my replace function to all cases (one or more characteres) 90% completed

--- my_replace_for_all_cases.py
+++ my_replace_for_all_cases.py
@@ -4,21 +4,26 @@
 def custom_replace(string, parameter, new_parameter):
     string_aux = ""
     searched_string = ""
+    index = 0
 
-    for index in range(len(string)):
+    while index < len(string):
         if string[index].lower() == parameter[0].lower():
             for sec_index in range(len(parameter)):
                 if string[index + sec_index].lower() == parameter[sec_index].lower():
                     searched_string += string[index + sec_index]
-            if searched_string == parameter:
+                else:
+                    string_aux += searched_string
+            if searched_string.lower() == parameter.lower():
                 string_aux += new_parameter
-
+                searched_string = ""
+                index += len(parameter)
         else:
             string_aux += string[index]
+            index += 1
 
     return string_aux
 
 a = raw_input()
+print custom_replace(a, "Jonathan", "Wesley")
 
-print a
-print custom_replace(a, "I am", "We are")
+# assert custom_replace("I am Jon. I am a boy", "I am", "We are") == "We are Jon. We are a boy"
