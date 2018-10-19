08b7161f0f6864666c7f50c0916eafd00fcbeb0b
jonathantvrs <jhonathan200969@gmail.com>
2018-10-19 04:26:29 -0300

    commiting my function count

---
+++ my_count.py
@@ -0,0 +1,16 @@
+def custom_count(list, parameter):
+    counter = 0
+
+    for element in list:
+        if element == parameter:
+            counter = counter + 1
+
+    return counter
+
+
+
+
+
+assert custom_count(["abc", 2, 123, "abc", "qwe"], "abc") == 2
+assert custom_count([123, 234, "abc", 345], 123) == 1
+assert custom_count(["abc"], "abc") == 1
