6b0fd3f10e30396619bce09fbcf724fdf1163d3a
jonathantvrs <jhonathan200969@gmail.com>
2018-10-19 04:31:02 -0300

    commiting my function count

--- my_count.py
+++ my_count.py
@@ -7,10 +7,6 @@ def custom_count(list, parameter):
 
     return counter
 
-
-
-
-
-assert custom_count(["abc", 2, 123, "abc", "qwe"], "abc") == 2
-assert custom_count([123, 234, "abc", 345], 123) == 1
-assert custom_count(["abc"], "abc") == 1
+# assert custom_count(["abc", 2, 123, "abc", "qwe"], "abc") == 2
+# assert custom_count([123, 234, "abc", 345], 123) == 1
+# assert custom_count(["abc"], "abc") == 1
