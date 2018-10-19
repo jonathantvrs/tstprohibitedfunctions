1209f3558e718749d44f16bcb7056db05abfba2c
jonathantvrs <jhonathan200969@gmail.com>
2018-10-19 03:34:08 -0300

    Add tests in function my_in

--- my_in.py
+++ my_in.py
@@ -6,3 +6,6 @@ def custom_in(list, parameter):
         if element == parameter:
             return True
         return False
+
+# assert custom_in(["a", "oam", "okay"],"b") == False
+# assert custom_in(["b", "aom", "okay"]) == True
