7740046909335c16aae1901c401dbf1ac51931ad
jonathantvrs <dev03@easyjur.com>
2018-12-16 03:03:32 -0300

    modifying my_replace_for_one_character prohibited function

--- my_replace_for_one_character.py
+++ my_replace_for_one_character.py
@@ -4,10 +4,8 @@
 def custom_replace(sequence, parameter, new_parameter):
 	new_sequence = ''
 	for element in sequence:
-		if parameter == element:
-			new_sequence += new_parameter
-		else:
-			new_sequence += element
+		if element == parameter: new_sequence += new_parameter 
+		else: new_sequence += element
 	
 	return new_sequence
 
