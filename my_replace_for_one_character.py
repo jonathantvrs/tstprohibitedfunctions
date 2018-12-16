3aed35567040161f201717302586b8972477d957
jonathantvrs <dev03@easyjur.com>
2018-12-16 02:53:53 -0300

    modifying my_replace_for_one_character prohibited function

--- my_replace_for_one_character.py
+++ my_replace_for_one_character.py
@@ -1,19 +1,14 @@
 # -*-  coding: utf-8  -*-
 # author: jonathantvrs
 
-def custom_replace(string, parameter, new_parameter):
-    string_aux = ""
+def custom_replace(sequence, parameter, new_parameter):
+	new_sequence = ''
+	for element in sequence:
+		if parameter == element:
+			new_sequence += new_parameter
+		else:
+			new_sequence += element
+	
+	return new_sequence
 
-    for element in string:
-        if element.lower() == parameter.lower():
-            string_aux += new_parameter
-        else:
-            string_aux += element
-
-    return string_aux
-
-
-# test = raw_input()
-# print custom_replace(test, ",", " ")
-
-# assert custom_replace("Rafael é feio", "a", "o") == "Rofoel é feio"
+assert custom_replace("Rafael é feio", "a", "o") == "Rofoel é feio"
