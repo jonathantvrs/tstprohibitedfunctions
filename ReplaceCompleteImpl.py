48765f52b5b176977cd5a4e8de39e07baad06e39
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2019-09-22 00:31:45 -0300

    delete bad implementation replace full

--- ReplaceCompleteImpl.py
+++ ReplaceCompleteImpl.py
@@ -1,37 +1,4 @@
 # -*-  coding: utf-8  -*-
 # author: jonathantvrs
 
-def custom_replace(string, parameter, new_parameter):
-    string_aux = ""
-    searched_string = ""
-    index = 0
-
-    while index < len(string):
-        if string[index].lower() == parameter[0].lower():
-            for sec_index in range(len(parameter)):
-                if (index + sec_index) < len(string):
-                    if string[index + sec_index].lower() == parameter[sec_index].lower():
-                        searched_string += string[index + sec_index]
-                    else:
-                        string_aux += searched_string
-                else:
-                    break
-
-            if searched_string.lower() == parameter.lower():
-                string_aux += new_parameter
-                searched_string = ""
-                index += len(parameter)
-            else:
-                index += len(searched_string)
-                string_aux += searched_string
-
-        else:
-            string_aux += string[index]
-            index += 1
-
-    return string_aux
-
-# assert custom_replace("Eu sou Jonathan. Me chamam de Jonathan. Mas prefiro Jon.", "Jonathan", "Wesley") == \
-#                        "Eu sou Wesley. Me chamam de Wesley. Mas prefiro Jon."
-# assert custom_replace("aaaaa", "a", "b") == "bbbbb"
-# assert custom_replace("I am Jon. I am a boy", "I am", "We are") == "We are Jon. We are a boy"
+def freplacefull(string, parameter, new_parameter):
